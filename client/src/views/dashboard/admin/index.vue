7a2514268555fe00a8cf6173693edb6d3d89f305
Marcelo de Almeida Magalhães <contato@magaweb.com.br>
2020-09-25 00:53:13 -0300

    dashboard

--- client/src/views/dashboard/admin/index.vue
+++ client/src/views/dashboard/admin/index.vue
@@ -12,49 +12,21 @@
       </el-col>
     </el-row><br>
     <el-row :gutter="20">
-      <el-col :span="8">
-        <el-card class="box-card" style="width: 100%;">
-          <div slot="header" class="clearfix">
-            <span @click="vai('ultimas vendas')">Total de vendas por dia</span>
-          </div>
-          <el-table
-            :key="tableKey"
-            v-loading="listLoading"
-            :data="list"
-           
-            highlight-current-row
-            style="width: 100%; _font-size: 18px;"
-            >
-            <el-table-column label="Data" prop="data" sortable="custom" align="center" width="115">
-              <template slot-scope="scope">
-                <span>{{ scope.row.data }}</span>
-              </template>
-            </el-table-column>
-
-            <el-table-column label="#" prop="n" sortable="custom" align="center" width="55">
-              <template slot-scope="scope">
-                <span>{{ scope.row.n }}</span>
-              </template>
-            </el-table-column>
-
-            <el-table-column label="Total" prop="total" sortable="custom" align="center" width="150">
-              <template slot-scope="scope">
-                <span>{{ scope.row.total | money }}</span>
-              </template>
-            </el-table-column>
-          
-          </el-table>
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card class="box-card" style="width: 100%; max-height: 500px;">
+    
+      <el-col :span="11">
+        <el-card class="box-card" style="width: 100%; height: 490px;">
           <div slot="header" class="clearfix">
             <span @click="vai('ultimas vendas')">Contas dos clientes</span>
           </div>
-          <el-table :data="lista2"  highlight-current-row style="width: 100%; font-size: 17px;">
+          <el-table :data="lista2" height="360" highlight-current-row style="width: 100%; font-size: 16px;">
             <el-table-column label="Cliente_nome" prop="cliente_nome" sortable="custom" align="center" width="200">
               <template slot-scope="scope">
                 <span>{{ scope.row.cliente_nome }}</span>
+              </template>
+            </el-table-column>
+             <el-table-column label="N" prop="faturas" sortable="custom" align="center" width="120">
+              <template slot-scope="scope">
+                <span>{{ scope.row.faturas }}</span>
               </template>
             </el-table-column>
             <el-table-column label="Total" prop="total" sortable="custom" align="center" width="150">
@@ -63,147 +35,37 @@
               </template>
             </el-table-column>
           </el-table>
+          <div style="text-align: right; font-size: 25px; margin-top: 10px; margin-right: 45px;">Total: {{ttt_}}</div>
         </el-card>
       </el-col>
-      <el-col :span="8">
-        <el-card class="box-card" style="width: 100%; max-height: 500px;">
+      <el-col :span="13">
+        <el-card class="box-card" style="width: 100%; height: 490px;">
           <div slot="header" class="clearfix">
-            <span @click="vai('ultimas vendas')">Movimento do caixa</span>
+            <span @click="vai('ultimas vendas')">Contas dos clientes (Percentuais)</span>
           </div>
-          <el-table :data="list_caixa"  fit highlight-current-row style="width: 100%; font-size: 17px;">
-            <el-table-column label="Data" prop="Data" sortable="custom" align="center" width="200">
-              <template slot-scope="scope">
-                <span>{{ scope.row.data }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="Desc" prop="desc" sortable="custom" align="center" width="150">
-              <template slot-scope="scope">
-                <span>{{ scope.row.desc}}</span>
-              </template>
-            </el-table-column>
-            <el-table-column label="Valor" prop="valor" sortable="custom" align="center" width="150">
-              <template slot-scope="scope">
-                <span>{{ scope.row.valor | money}}</span>
-              </template>
-            </el-table-column>
-          </el-table>
+           <apexchart type="pie" width="380" :options="chartOptions_2" :series="series_2"></apexchart>
         </el-card>
       </el-col>
+     
     </el-row><br>
-    
     <el-row :gutter="20">
-      <el-col :span="8">
-        <el-card class="box-card" style="width: 100%;">
-          <div slot="header" class="clearfix">
-            <span @click="vai('ultimas vendas')">Total de vendas por dia</span>
-          </div>
-          <bar-chart />
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card class="box-card" style="width: 100%; max-height: 400px;">
-          <div slot="header" class="clearfix">
-            <span @click="vai('ultimas vendas')">Contas dos clientes</span>
-          </div>
-           <pie-chart />
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card class="box-card" style="width: 100%; max-height: 400px;">
-          <div slot="header" class="clearfix">
-            <span @click="vai('ultimas vendas')">Movimento do caixa</span>
-          </div>
-           <line-chart :chart-data="vendas" />
-        </el-card>
-      </el-col>
     </el-row>
     <br>
-    <!-- <github-corner class="github-corner" />
-
-    <panel-group @handleSetLineChartData="handleSetLineChartData" />
-
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData" />
-    </el-row>
-
-    <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart />
-        </div>
-      </el-col>
-    </el-row>
-
-    <el-row :gutter="8">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
-        <transaction-table />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <todo-list />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <box-card />
-      </el-col>
-    </el-row> -->
   </div>
 </template>
 
 <script>
-// import GithubCorner from '@/components/GithubCorner'
-// import PanelGroup from './components/PanelGroup'
-import LineChart from './components/LineChart'
-// import RaddarChart from './components/RaddarChart'
-import PieChart from './components/PieChart'
-import BarChart from './components/BarChart'
-// import TransactionTable from './components/TransactionTable'
-// import TodoList from './components/TodoList'
-// import BoxCard from './components/BoxCard'
+Number.prototype.format = function(n, x, s, c) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+        num = this.toFixed(Math.max(0, ~~n));
 
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 100, 100, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
+    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
 import { fetchList } from '@/api/generic'
 import Pagination from '@/components/Pagination'
-import Teste from './components/Teste'
 export default {
   name: 'DashboardAdmin',
-  components: { Teste, Pagination, BarChart, PieChart, LineChart },
-  // components: {
-  //   GithubCorner,
-  //   PanelGroup,
-  //   LineChart,
-  //   RaddarChart,
-  //   PieChart,
-  //   BarChart,
-  //   TransactionTable,
-  //   TodoList,
-  //   BoxCard
-  // },
+  components: { Pagination },
   filters: {
      data(value){
       return moment(+value).format('DD-MM-YYYY, hh:mm:ss a')
@@ -223,30 +85,27 @@ export default {
   },
   data() {
     return {
-       chartOptions: {
-            dataLabels: {
-              enabled: true,
-            },
-          chart: {
-            id: 'vuechart-example'
-          },
-          xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-          }
+      ttt_: 0,
+      chartOptions: {
+       
+        chart: {
+          id: 'vuechart-example'
         },
-        series: [{
-          name: 'series-1',
-          data: [30, 40, 35, 50, 49, 60, 70, 91]
-        }],
-       vendas: {faturado:[], total:[]},
-       lineChartData: lineChartData.newVisitis,
-       lista2: [],
-       listLoading: true,
-       tableKey: 0,
-       total: 0,
-       list_caixa: [],
-       list: [],
-        listQuery: {
+        
+      },
+      series: [],
+      series_2: [],
+      chartOptions_2: {
+          labels: []
+      },
+      vendas: {faturado:[], total:[]},
+      lista2: [],
+      listLoading: true,
+      tableKey: 0,
+      total: 0,
+      list_caixa: [],
+      list: [],
+      listQuery: {
         tipo: 1,
         page: 1,
         limit: 7,
@@ -262,25 +121,61 @@ export default {
     this.getList()
   },
   methods: {
+    getSummaries(param) {
+        const { columns, data } = param;
+        const sums = [];
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            sums[index] = 'Total';
+            return;
+          }
+          const values = data.map(item => Number(item[column.property]));
+          if (!values.every(value => isNaN(value))) {
+           if (index === 2) {
+              sums[index] =  'R$ ' + values.reduce((prev, curr) => {
+                  const value = Number(curr);
+                  if (!isNaN(value)) {
+                    return prev + curr;
+                  } else {
+                    return prev;
+                  }
+                }, 0).format(2, 3, '.', ',');
+           }else{
+             sums[index] =  values.reduce((prev, curr) => {
+                  const value = Number(curr);
+                  if (!isNaN(value)) {
+                    return prev + curr;
+                  } else {
+                    return prev;
+                  }
+                }, 0);
+           }
+          } else {
+            sums[index] = 'N/A';
+          }
+        });
+
+        return sums;
+      },
+  
+  
     vai(local){
       if (local == 'ultimas vendas') {
         this.$router.push('/relatorios/vendas_total');
       }
     },
     getList() {
-      // const changeValue = (list, prop) => {
-      //   return list.map(item => {
-      //     const obj = Object.assign({}, item);
-      //     obj[prop] = "$" + obj[prop].toFixed(2)
-      //     return obj;
-      //   });
-      // }
       this.listLoading = true
       const self = this
       fetchList('rel_vendas_total', this.listQuery).then(response => {
         this.list = response.data.items.reverse()
+        
+        
+
+
         this.chartOptions = {
-         chart: {
+          chart: {
+            id: 'vuechart-example',
             height: '350',
             width: '100%',
             type: 'bar',
@@ -288,30 +183,30 @@ export default {
             zoom: {
               enabled: true,
               type: 'x'
-            }, dropShadow: {
-                enabled: true,
-                color: '#000',
-                top: 18,
-                left: 7,
-                blur: 10,
-                opacity: 0.2
-              },
+            }, 
+            dropShadow: {
+              enabled: true,
+              color: '#000',
+              top: 18,
+              left: 7,
+              blur: 10,
+              opacity: 0.2
+            }
           }, 
-           plotOptions: {
-              bar: {
-                horizontal: false,
-                columnWidth: '85%',
-               
-              },
-            },
-             dataLabels: {
-              enabled: true
-            },
-           tooltip: {
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '85%',
+            }
+          },
+          dataLabels: {
+            enabled: true
+          },
+          tooltip: {
               followCursor: true,
               shared: false
             },
-            markers: {
+          markers: {
               size: 0,
               fillOpacity: 0,
               strokeOpacity: 0
@@ -322,12 +217,12 @@ export default {
           xaxis: {
             categories: this.list.map(function(x){return x.data})
           },
-           yaxis: {
-              title: {
-                text: '$ (Valores)'
-              },
-              max: Math.max(...this.list.map(function(x){return x.total.toFixed(2)})),
+          yaxis: {
+            title: {
+              text: '$ (Valores)'
             },
+            max: Math.max(...this.list.map(function(x){return x.total.toFixed(2)})),
+          },
         }
         this.series = [{
           name: 'Total',
@@ -342,24 +237,142 @@ export default {
           type: "bar",
           data: this.list.map(function(x){return x.faturado.toFixed(2)})
         }]
-        //this.series.data = this.list.map(function(x){return x.faturado})
-        // this.vendas.faturado = this.list.map(function(x){return x.faturado})
-        // this.vendas.avista = this.list.map(function(x){return x.dinheiro+x.cartao})
         this.total = response.data.total
         this.listLoading = false
         console.log('this.vendas.total:', this.vendas.total);
       })
-      
 
       fetchList(`
         select f.cliente cliente_id, c.nome cliente_nome, count() faturas, sum(f.valor) total 
         from faturados f, clientes c 
         WHERE f.cliente = c.id
         GROUP by f.cliente
-        Order by total limit 7`, {tipo: 'sql'}).then(response => {
+        Order by total`, {tipo: 'sql'})
+      .then(response => {
         this.lista2 = response.data.items
-        // this.lista2 = changeValue(this.lista2, this.lista2.total)
-        console.log('lista2:', this.lista2);
+        console.log('this.lista2>>:', this.lista2);
+        var t1 = this.lista2.map(function(x){return +x.total.toFixed(2)*-1})
+        console.log('t1:>', t1);
+        console.log('!!!?>', this.lista2.map(function(x){return +x.total.toFixed(2)*-1}));
+        this.ttt_ = 'R$ ' + response.data.items.map(function(x){return +x.total}).reduce((a, b) => {
+          return a + b
+        }, 0).format(2, 3, '.', ',').toString();
+        //Separa os 5 primeiros
+        var lista_5_nomes = this.lista2.slice(0,7).map(function(x){return x.cliente_nome})
+        var lista_5_valores = this.lista2.slice(0,7).map(function(x){return +x.total.toFixed(2)*-1})
+        
+        var ttt0 = this.lista2.slice(7).map(function(x){return +x.total*-1}).reduce((a, b) => {
+          return a + b
+        }, 0).toFixed(2)
+
+        var lista_completo_nomes = [...lista_5_nomes, 'outros']
+        var lista_completo_valores = [...lista_5_valores, +ttt0]
+        
+        console.log('lista_completa:>', lista_completo_nomes, lista_completo_valores);
+
+        // var ttt0 = this.lista2.map(function(x){return +x.total*-1}).reduce((a, b) => {
+        //       return a + b
+        //     }, 0).toFixed(2)
+
+        // console.log('ttt0:', ttt0);
+
+        // var ttt_nomes = this.lista2.slice(0,5).map(function(x){return x.cliente_nome}).reduce((a, b) => {
+        //       return a + b
+        //     }, 0).toFixed(2)
+
+        // console.log('ttt_nomes:', ttt_nomes);
+
+        //  var ttt = this.lista2.slice(0,5).map(function(x){return +x.total*-1}).reduce((a, b) => {
+        //       return a + b
+        //     }, 0).toFixed(2)
+
+        // console.log('ttt:', ttt);
+
+
+        // var ttt2 = this.lista2.slice(5).map(function(x){return +x.total*-1}).reduce((a, b) => {
+        //       return a + b
+        //     }, 0).toFixed(2)
+
+        
+        // console.log('ttt2:', ttt2);
+        this.series_2 = lista_completo_valores
+        this.chartOptions_2 = {
+          chart: {
+            width: 550,
+            type: 'donut',
+          },
+          plotOptions: {
+            pie: {
+              size: 550
+            },
+             donut: {
+      size: '65%',
+      background: 'transparent',
+      labels: {
+        show: false,
+        name: {
+          show: true,
+          fontSize: '22px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 600,
+          color: undefined,
+          offsetY: -10,
+          formatter: function (val) {
+            return val
+          }
+        },
+        value: {
+          show: true,
+          fontSize: '20px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 400,
+          color: undefined,
+          offsetY: 16,
+          formatter: function (val) {
+            return val
+          }
+        },
+        total: {
+          show: true,
+          showAlways: true,
+          label: 'Total',
+          fontSize: '22px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 600,
+          color: '#373d3f',
+          formatter: function (w) {
+            return this.lista2.map(function(x){return +x.total*-1}).reduce((a, b) => {
+              return a + b
+            }, 0).toFixed(2)
+          }
+        }
+      }
+    },      
+  },
+         
+          labels: lista_completo_nomes,
+          
+        }
+        // this.series_2 = this.lista2.map(function(x){return +(x.total).toFixed(2)})
+        // this.chartOptions_2 = {
+        //     chart: {
+        //       width: 380,
+        //       type: 'pie',
+        //     },
+        //     labels: this.lista2.map(function(x){return x.cliente_nome}),
+        //     responsive: [{
+        //       breakpoint: 480,
+        //       options: {
+        //         chart: {
+        //           width: 200
+        //         },
+        //         legend: {
+        //           position: 'bottom'
+        //         }
+        //       }
+        //     }]
+        //   }
+      
       })
 
       fetchList('view_fin_caixa', this.listQuery).then(response => {
